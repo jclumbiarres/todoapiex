630b120e974f30eba52b0cdb59fe61cc1810e17e
Juan Carlos Lumbiarres <jclumbiarres@protonmail.com>
2024-07-05 21:58:07 +0200

    fix: POST y GET de Todos funcionando

--- lib/todoapiex/router.ex
+++ lib/todoapiex/router.ex
@@ -11,13 +11,44 @@ defmodule Todoapiex.Router do
     json_decoder: Jason
   )
 
+  # get "/todos" do
+  #   todos = Todo.listar()
+  #   todos_json = Jason.encode!(todos)
+  #   send_resp(conn, 200, todos_json)
+  # end
   get "/todos" do
     todos = Todo.listar()
     todos_json = Jason.encode!(todos)
     send_resp(conn, 200, todos_json)
   end
 
+  post "/todos" do
+    case Plug.Conn.read_body(conn) do
+      {:ok, body, _conn} ->
+        case Jason.decode(body) do
+          {:ok, todo} ->
+            Todo.anadir(conn, todo)
+            send_resp(conn, 201, "Ok")
+
+          {:error, _error} ->
+            IO.puts(body)
+            send_resp(conn, 400, "Invalid JSON payload")
+        end
+
+      {:error, _reason} ->
+        send_resp(conn, 500, "Error reading request body")
+    end
+  end
+
   match _ do
     send_resp(conn, 404, "oops")
+  end
+end
+
+defimpl Jason.Encoder, for: Todoapiex.Todo do
+  def encode(%Todoapiex.Todo{titulo: _titulo, desc: _desc, completado: _completado} = todo, opts) do
+    Map.from_struct(todo)
+    |> Map.take([:titulo, :desc, :completado])
+    |> Jason.Encode.map(opts)
   end
 end
